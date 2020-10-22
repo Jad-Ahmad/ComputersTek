b7a59f0c54c34b61c37bbde7630fc1e549b5ee79
Jad-Ahmad <61512454+Jad-Ahmad@users.noreply.github.com>
2020-10-21 21:01:41 -0400

    second

---
+++ web.js
@@ -0,0 +1,13 @@
+// var msg = "hello world";
+// console.log(msg);
+
+//  var char = msg.charAt(1);
+
+// console.log(char);
+
+
+  function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+  }
+
